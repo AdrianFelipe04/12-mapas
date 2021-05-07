07ec85647a2c59d04b5989d7eb1cf17224b9a0f7
DESKTOP-SV3DJEM\UserQV <acruz@qvision.com.co>
2021-05-07 14:02:06 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+import { MatSliderModule } from '@angular/material/slider';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module';
+import { MapaComponent } from './compnents/mapa/mapa.component';
+
+//maps
+// import { AgmCoreModule } from '@agm/core';
+
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MapaComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatSliderModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    // AgmCoreModule.forRoot({
+    //   apiKey: 'AIzaSyDNOu2JQ001PxZY-GVwFvVou0_6h_Sj-14'
+    // })
   ],
   providers: [],
   bootstrap: [AppComponent]
